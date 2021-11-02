fe87aa1b84d82d3b7d74483afbf67d301ad79f4a
daw2-molins22 <79204486+daw2-molins22@users.noreply.github.com>
2021-11-02 22:51:16 +0100

    Add files via upload

---
+++ N3P301AlejandroMolinscomments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Documet</title>
+</head>
+<body>
+<ol>	
+	<li>Insertar datos datos mediante PHP a MySQL</li>
+	<li>Crear unas tablas</li>
+	<li>Introducir comentarios y resenyas</li>
+	<li>Calcular la media de las resenyas</li>
+	<li>Ordenar por fechas los comentarios</li>
+	<li>Poner en cada linea par e impar colores</li>
+
+</ol>
+<p>Un 9 y un 10.</p>
+
+<p>La nota que me pongo es un 9</p>
+
+<p>Explicacion un poco mas adaptada y detallada para el alumnado</p>
+</body>
+</html>
+	
